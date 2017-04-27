d5f3de44ebf5311c2b828fb3c28c5133db4c22ad
Andrew Glines and David Wilson <david@>
2017-04-27 10:25:09 -0700

    complete backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,51 @@
+//BACK END
+var areTheyOver21 = function(dateOfBirth){
+  var userYear = parseInt(dateOfBirth.slice(0,4));
+  var userMonth = parseInt(dateOfBirth.slice(5,7));
+  var userDay = parseInt(dateOfBirth.slice(8,10));
+  // console.log("userYear = ", userYear);
+  // console.log("userMonth= ", userMonth);
+  // today's date
+  var today = new Date();
+  var todayDay = parseInt(today.getDate());
+  var todayMonth = parseInt(today.getMonth()+1);
+  var todayYear= parseInt(today.getFullYear());
+  //calculation
+
+  if (todayYear - userYear < 21){
+    // areTheyOver21 = "fail"
+    var is21 = false;
+    return is21;
+  }
+
+  if (todayYear - userYear > 21){
+    var is21 = true;
+    return is21;
+  }
+
+  if (todayYear - userYear === 21 ) {
+    console.log("Year fail");
+    //check month to see if it's over ...
+    if (todayMonth - userMonth === 0) {
+      console.log("Month fail");
+      //check day to see if its over ...
+      if (todayDay - userDay ===0 ) {
+        console.log("happy birthday!  Find a designated driver.");
+        var is21 = true;
+        return is21;
+      }
+    }
+  }
+
+};
 //FRONT END
 $(document).ready(function(){
-
   $("form#calculateDob").submit(function(event){
     event.preventDefault();
     var dateOfBirth = $("#born").val();
-    console.log(typeof(dateOfBirth), dateOfBirth);
-    console.log(dateOfBirth.slice(0,4));
-    console.log(dateOfBirth.slice(5,7));
-    console.log(dateOfBirth.slice(8,10));
+    var result = areTheyOver21(dateOfBirth);
+    console.log(result, typeof(result));
+    $("output").text(result);
+    // console.log(success);
   });
 });
-
-//BACK END
-var userYear = parseInt(dateOfBirth.slice(0,4));
-var userMonth = parseInt(dateOfBirth.slice(5,7));
-var userDay = parseInt(dateOfBirth.slice(8,10));
-
-// today's date
-var today = new Date();
-var todayDay = parseInt(today.getDate());
-var todayMonth = parseInt(today.getMonth()+1);
-var todayYear= parseInt(today.getFullYear());
-
-//calculation
-if (todayYear - userYear <= 21 ) {
-  //check month to see if it's over ...
-  else if (todayMonth - userMonth <= 0) {
-      else if (todayDay - userDay <=0 ) {
-
-      }
-      }
-  }
-  }
-}
